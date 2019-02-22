2d1e052535720bb0a9d518701e45f4323a1e4071
YocelinGRomero <garcia_romero.y@hotmail.com>
2019-02-22 16:20:20 -0600

    Adding test

--- src/components/StarsComponent.vue
+++ src/components/StarsComponent.vue
@@ -1,50 +1,61 @@
 <template>
-<div>
-    <div v-for="repo in user.starredRepositories && user.starredRepositories.edges" :key="repo.node.id" class="star row">
+  <div>
+    <div
+      v-for="repo in user.starredRepositories && user.starredRepositories.edges"
+      :key="repo.node.id"
+      class="star row"
+    >
       <div class="col-md-8">
-        <span class="userRepo">{{ repo.node.owner.login}} / </span>
-        <a :href="repo.node.url" class="userRepo">{{ repo.node.name}}</a>
-        <p class="description">{{ repo.node.description}}</p>
-        <span v-for="topic in repo.node.repositoryTopics.nodes" :key="topic.id" class="topic">
-          {{topic.topic.name}}
+        <span class="userRepo">{{ repo.node.owner.login }} / </span>
+        <a :href="repo.node.url" class="userRepo">{{ repo.node.name }}</a>
+        <p class="description">{{ repo.node.description }}</p>
+        <span
+          v-for="topic in repo.node.repositoryTopics.nodes"
+          :key="topic.id"
+          class="topic"
+        >
+          {{ topic.topic.name }}
         </span>
         <span class="topic">{{ repo.starredAt }}</span>
       </div>
       <div class="col-md-3">
-        <button class="btn btn-light" @click="removeStar(repo.node.id)"><i class='far fa-star'></i> Unfollow</button>
+        <button class="btn btn-light" @click="removeStar(repo.node.id)"
+          ><i class="far fa-star"></i> Unfollow</button
+        >
       </div>
     </div>
-</div>
+  </div>
 </template>
 <script>
-import gql from 'graphql-tag';
+import gql from 'graphql-tag'
 
 const starredByUser = gql`
   query user {
-  user(login: "YocelinGR"){
-    id
-    starredRepositories(first:10){
-      totalCount
-      edges{
-        starredAt
-        node{
-          id
-          name
-          description
-          owner{
-            login
+    user(login: "YocelinGR") {
+      id
+      starredRepositories(first: 10) {
+        totalCount
+        edges {
+          starredAt
+          node {
             id
-          }
-          createdAt
-          url
-          repositoryTopics(first: 3){
-            nodes{
+            name
+            description
+            owner {
+              login
               id
-              resourcePath
-              url
-              topic{
+            }
+            createdAt
+            url
+            repositoryTopics(first: 3) {
+              nodes {
                 id
-                name
+                resourcePath
+                url
+                topic {
+                  id
+                  name
+                }
               }
             }
           }
@@ -52,72 +63,71 @@ const starredByUser = gql`
       }
     }
   }
-}
-`;
+`
 const removingStar = gql`
-  mutation removeStar($input: RemoveStarInput!){
-  removeStar(input: $input) {
-    clientMutationId
-    starrable{
-      id
+  mutation removeStar($input: RemoveStarInput!) {
+    removeStar(input: $input) {
+      clientMutationId
+      starrable {
+        id
+      }
     }
   }
-}
-`;
+`
 export default {
-  data: ()=>({
-    user: {}
+  data: () => ({
+    user: {},
   }),
   methods: {
-    removeStar(id){
+    removeStar(id) {
       const varStar = {
-              starrableId: id,
-              clientMutationId:  "MDQ6VXNlcjM5ODMzMDQ1"
-            };
-      location.reload();
+        starrableId: id,
+        clientMutationId: 'MDQ6VXNlcjM5ODMzMDQ1',
+      }
       this.$apollo.mutate({
         mutation: removingStar,
         variables: {
-            input: varStar,
-        }
+          input: varStar,
+        },
       })
-    }
+      location.reload()
+    },
   },
   apollo: {
     user: {
       query: starredByUser,
-    }
-  }
+    },
+  },
 }
 </script>
 <style scoped>
-  .star {
-    justify-content: center;
-    align-items: center;
-    margin: 15px;
-    padding-bottom: 10px;
-    border-bottom: rgb(187, 185, 185) solid 1px;
-  }
-  .btn {
-    background-color: #f0f1f1;
-    border-color: #a5a6a7;
-  }
-  .btn:hover {
-    background-color: #ccd2db;
-  }
-  .userRepo {
-    font-size: 20px;
-    color: rgb(10, 112, 196);
-    padding: 4px;
-  }
-  .description {
-    font-size: 18px;
-    color: rgb(136, 137, 138);
-    padding: 2px;
-  }
-  .topic {
-    font-size: 14px;
-    color: rgb(70, 71, 71);
-    padding: 6px;
-  }
+.star {
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 10px;
+  margin: 15px;
+  border-bottom: rgb(187, 185, 185) solid 1px;
+}
+.btn {
+  background-color: #f0f1f1;
+  border-color: #a5a6a7;
+}
+.btn:hover {
+  background-color: #ccd2db;
+}
+.userRepo {
+  padding: 4px;
+  font-size: 20px;
+  color: rgb(10, 112, 196);
+}
+.description {
+  padding: 2px;
+  font-size: 18px;
+  color: rgb(136, 137, 138);
+}
+.topic {
+  padding: 6px;
+  font-size: 14px;
+  color: rgb(70, 71, 71);
+}
 </style>
---
+++ src/main.js
@@ -0,0 +1,70 @@
+import Vue from 'vue'
+import App from './app'
+import router from '@router'
+import store from '@state/store'
+import '@components/_globals'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import VueApollo from 'vue-apollo'
+
+// Don't warn about using the dev version of Vue in development.
+Vue.config.productionTip = false // process.env.NODE_ENV === 'production'
+
+// If running inside Cypress...
+if (window.Cypress) {
+  // Ensure tests fail when Vue emits an error.
+  Vue.config.errorHandler = window.Cypress.cy.onUncaughtException
+}
+
+const USERNAME = 'YocelinGR'
+const TOKEN = '22faa3d55efae03aee72c12d282a3c510444b5f5'
+
+// Create apollo client
+const httpLink = new HttpLink({
+  uri: `https://api.github.com/graphql?user_name=${USERNAME}&access_token=${TOKEN}`,
+})
+
+const appCache = new InMemoryCache()
+const headers = new Headers()
+
+headers.append('Content-Type', 'appication/json')
+headers.append('Access-Control-Allow-Origin', 'http://localhost:8080')
+headers.append('Access-Control-Allow-Credentials', 'true')
+
+const middlewareLink = setContext(() => ({
+  headers,
+}))
+
+const link = middlewareLink.concat(httpLink)
+const apolloClient = new ApolloClient({
+  link: link,
+  cache: appCache,
+  connectToDevTools: true,
+})
+
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
+
+export default apolloClient
+
+// Start the app
+const app = new Vue({
+  el: '#app',
+  router,
+  store,
+  apolloProvider,
+  render: (h) => h(App),
+})
+
+// If running inside Cypress...
+if (window.Cypress) {
+  // Attach the app to the window, which can be useful
+  // for manually setting state in Cypress commands
+  // such as `cy.logIn()`.
+  window.__app__ = app
+}
